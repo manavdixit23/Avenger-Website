08ea095fbf35b40a9001a02422a488e515785f6b
Manav Dixit <61451540+manavdixit23@users.noreply.github.com>
2021-07-26 17:36:24 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,9 @@
+
+$('#bgvid').click(function(){
+	this.paused?this.play():this.pause();
+	// $(this).attr("src","Templates/logo.png");
+	// var vid = $("video").html();
+	// console.log(vid);
+	// $('#bgvid').fadeOut(1000);
+	// $('#bgvid').fadeIn(2000);
+});
